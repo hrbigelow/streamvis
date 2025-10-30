de14543795cb7c3e614600844c30cb4c09c042e2
Henry Bigelow <hrbigelow@gmail.com>
2025-10-29 18:00:54 -0700

    minor fixes to python client script

--- client/streamvis/script.py
+++ client/streamvis/script.py
@@ -19,20 +19,44 @@ from google.protobuf.empty_pb2 import Empty
 
 
 def demo_sync_fn(
-    grpc_uri: str, scope: str, delete_existing_names: bool=True, num_steps: str="2000"):
+    grpc_uri: str, 
+    scope: str, 
+    delete_existing_names: bool=True, 
+    num_steps: int=2000,
+    report_every: int=100
+):
     num_steps = int(num_steps)
     log_data(grpc_uri, scope, delete_existing_names, num_steps)
 
 def demo_async_fn(
-    grpc_uri: str, scope: str, delete_existing_names: bool=True, num_steps: str="2000"):
+    grpc_uri: str, 
+    scope: str, 
+    delete_existing_names: bool=True, 
+    num_steps: int=2000,
+    report_every: int=100
+):
     num_steps = int(num_steps)
     asyncio.run(log_data_async(grpc_uri, scope, delete_existing_names, num_steps))
 
 
-def fetch(uri: str, scope: str, name: str):
+"""
+fetch and fetch_with_patterns return a dict with:
+    keys: (scope, name, index)
+    values: cds_data
+
+    cds_data is a dict with:
+       keys: axis names
+       values: numpy array with shape [index, point]
+"""
+def fetch(uri: str, scope: str, name: str) -> dict[tuple, 'cds_data']:
     return fetch_with_patterns(uri=uri, scope_pattern=f"^{scope}$", name_pattern=f"^{name}$")
 
-def fetch_with_patterns(uri: str, scope_pattern: str, name_pattern: str):
+def fetch_with_patterns(
+    uri: str, 
+    scope_pattern: str, 
+    name_pattern: str,
+    flat_format: bool=True
+) -> dict[tuple, 'cds_data']:
     try:
         channel = grpc.insecure_channel(uri)
         stub = pb_grpc.ServiceStub(channel)
@@ -53,24 +77,37 @@ def fetch_with_patterns(uri: str, scope_pattern: str, name_pattern: str):
                     raise ValueError(
                         f"QueryRecords should only return index or data.  Returned {other}")
 
-        return util.data_to_cds_flat(record, datas)
+        if flat_format:
+            return util.data_to_cds_flat(record, datas)
+        else:
+            return util.data_to_cds(record, datas)
     except grpc.RpcError as e:
         raise ValueError(f"RPC failed: {e.code()}: {e.details()}")
     finally:
         channel.close()
+
+def liftover(
+    source_uri: str,
+    target_uri: str,
+    scope_pattern: str
+):
+    """Transfers the content of scopes matching `scope_pattern` from `source_uri` to
+    `target_uri`
+    """
+    results = fetch_with_patterns(source_uri, scope_pattern, ".*", flat_format=False)
+    for scope, names in results.items():
+        import pdb
+        pdb.set_trace()
+        logger = DataLogger(scope, target_uri, tensor_type="numpy",
+                            delete_existing_names=True)
 
 
 def scopes(uri: str) -> list[str]:
     channel = grpc.insecure_channel(uri)
     stub = pb_grpc.ServiceStub(channel)
     scopes = []
-    for msg in stub.Scopes(Empty()):
-        match msg.WhichOneof("value"):
-            case "tag":
-                scopes.append(msg.tag.scope)
-            case other:
-                raise ValueError(
-                    f"Scopes() should only return Tag object.  Returned {other}")
+    for res in stub.Scopes(Empty()):
+        scopes.append(res.scope)
 
     return scopes
 
@@ -78,29 +115,24 @@ def scopes(uri: str) -> list[str]:
 def names(uri: str, scope: str) -> list[str]:
     channel = grpc.insecure_channel(uri)
     stub = pb_grpc.ServiceStub(channel)
-    req = pb.ScopeRequest(scope=scope)
+    req = pb.NamesRequest(scope=scope)
     names = []
-    for msg in stub.Names(req):
-        match msg.WhichOneof("value"):
-            case "tag":
-                names.append(msg.tag.name)
-            case other:
-                raise ValueError(
-                    f"Names() should only return Tag object.  Returned {other}")
+    for tag in stub.Names(req):
+        names.append(tag.name)
     return names 
 
 def config(uri: str, scope: str) -> dict[str, Any]:
     channel = grpc.insecure_channel(uri)
     stub = pb_grpc.ServiceStub(channel)
-    req = pb.ScopeRequest(scope=scope)
+    req = pb.ConfigRequest(scope=scope)
     configs = []
 
-    for msg in stub.Configs(req):
-        match msg.WhichOneof("value"):
+    for res in stub.Configs(req):
+        match res.WhichOneof("value"):
             case "index":
-                index = util.Index.from_record_result(msg.index)
+                index = util.Index.from_record_result(res.index)
             case "config":
-                configs.append(msg.config)
+                configs.append(res.config)
     return util.export_configs(index, configs)
 
 
@@ -132,14 +164,15 @@ def main():
         pprint(out)
 
     tasks = { 
-            "web-serve": serve,
-            "grpc-serve": grpc_serve,
-            "logging-demo": demo_sync_fn,
-            "logging-demo-async": demo_async_fn,
-            "scopes": partial(print_list, scopes),
-            "names": partial(print_list, names),
-            "counts": counts,
-            "config": partial(print_dict, config),
+             "web-serve": serve,
+             "grpc-serve": grpc_serve,
+             "logging-demo": demo_sync_fn,
+             "logging-demo-async": demo_async_fn,
+             "scopes": partial(print_list, scopes),
+             "names": partial(print_list, names),
+             "counts": counts,
+             "config": partial(print_dict, config),
+             "liftover": liftover,
             }
     fire.Fire(tasks)
 
